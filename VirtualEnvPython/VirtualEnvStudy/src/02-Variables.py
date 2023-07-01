15b60a594c2630755db0e423b477d48683114e1a
Felipe Gutierrez <felipe.madrid@yahoo.es>
2023-07-01 17:03:28 -0500

    Se agrega type() para saber el tipo de variable

--- VirtualEnvPython/VirtualEnvStudy/src/02-Variables.py
+++ VirtualEnvPython/VirtualEnvStudy/src/02-Variables.py
@@ -1,7 +1,17 @@
 # Variables Enteras / Int
-num = 5
-print(num)
-
+numero = int(5)
+print("Este es un Numero Entero",type(numero)) # type() me muestra que tipo de variable es lo que hay entre ()
+    # Resultado
+        # Este es un Numero Entero <class 'int'>
+    
 # Variables Decimales / Float
-num = 5.4321
-print(num)
+decimal = float(5.4321)
+print("Este es un Numero Float",type(decimal)) # type() me muestra que tipo de variable es lo que hay entre ()
+    # Resultado
+        # Este es un Numero Float <class 'float'>
+        
+# Variable String o Caracter
+caracter = str(30)
+print("Este es un Caracter",type(caracter)) # type() me muestra que tipo de variable es lo que hay entre ()
+    # Resultado
+        # Este es un Caracter <class 'str'>
