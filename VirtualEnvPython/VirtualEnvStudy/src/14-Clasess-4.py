ba84fd382865261176bc593d0d55364c91bb2372
Felipe Gutierrez <felipe.madrid@yahoo.es>
2023-07-05 15:18:23 -0500

    Classes 4 y 5

--- VirtualEnvPython/VirtualEnvStudy/src/14-Clasess-4.py
+++ VirtualEnvPython/VirtualEnvStudy/src/14-Clasess-4.py
@@ -97,5 +97,47 @@
 # restaurante2.set_direccion("Girardota")
 # restaurante2.get_direccion()  
 
-## HERENCIA
-##
+### HERENCIA
+    # Para este caso la Clase padre se llama Restaurante
+class Restaurante: # Clase Padre
+    
+    def __init__(self,nombre,telefono,direccion): 
+        self.nombre = nombre
+        self.telefono = telefono
+        self.direccion = direccion
+    
+    def monstrar_informacion(self):
+        print("\r")
+        print(f"Nombre: {self.nombre}")
+        print(f"Telefono: {self.telefono}")
+        print(f"Direccion: {self.direccion}")
+        print("\r")
+    
+restaurante = Restaurante("IlForno",604,"Poblado")
+restaurante.monstrar_informacion()
+
+restaurante2 = Restaurante("Rancherito",604,"Copacabana")
+restaurante2.monstrar_informacion()
+
+
+## Clase Hijo(Hotel) de clase Padre (Restaurante)
+    # Estructura
+        # class Hotel(Restaurante):
+        #    def __init__(self, nombre, telefono, direccion):
+        #        super().__init__(nombre, telefono, direccion)
+    
+        # hotel = Hotel("Hotel Rancherito",604,"Caldas")
+
+        # hotel.monstrar_informacion()
+
+class Hotel(Restaurante):
+    def __init__(self, nombre, telefono, direccion):
+        super().__init__(nombre, telefono, direccion)
+        
+hotel = Hotel("Hotel Rancherito",604,"Caldas")
+hotel.monstrar_informacion()
+hotel2 = Hotel("Hotel IlForno",604,"Belen")
+hotel2.monstrar_informacion()
+
+
+
